8faa45cd421bd69b657e70d67f8f86a88c277098
MatthewsMary <161976190+MatthewsMary@users.noreply.github.com>
2024-03-02 14:06:05 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const red = 254;
+const green = 234;
+const blue = 232;
+
+const header = document.getElementById("header");
+console.log(header.innerText);
+
+function changeColor() {
+    const scrollFactor = 1 + window.scrollY / 400;
+
+    const r = red / scrollFactor;
+    const g = green / scrollFactor;
+    const b = blue / scrollFactor;
+
+    const updatedColor = `rgb (${r}, ${g}, ${b})`;
+
+    header.style.backgroundColor = updatedColor;
+}
+changeColor()
+
+window.addEventListener('scroll', changeColor)
